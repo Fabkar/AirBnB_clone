ec63628fd73918101ee76310bdd65ea4a76cab4e
1uiscalderon <1857@holbertonschool.com>
2020-10-29 18:15:39 -0500

    Added 4th task

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ This module will define all common attributes/methods for other classes
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     Base class for all common attributes/methods for other classes
@@ -13,22 +14,34 @@ class BaseModel:
         """
         initialize class BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
         Representation
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
     def save(self):
         """
         Updates
         """
         self.updated_at = datetime.today()
-    
+
     def to_dict(self):
         """
         Dictionary
