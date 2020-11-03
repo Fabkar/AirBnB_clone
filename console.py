5625f73a5016455372ca934491379b87435bd7b4
1uiscalderon <1857@holbertonschool.com>
2020-11-03 11:06:23 -0500

    Check something

--- console.py
+++ console.py
@@ -23,13 +23,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Quit command to exit the program
-        """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, line):
-        """Exit Success
-        """
+        """Exit Success"""
         return True
 
     def emptyline(self):
